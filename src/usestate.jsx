0a153e53d2e02368787b6fd815b67fa1fb81f9e1
urooj <uroojismail48@gmail.com>
2026-05-02 01:37:10 +0500

    toggle,add , delte

--- src/usestate.jsx
+++ src/usestate.jsx
@@ -1,35 +1,82 @@
-
+import React, { useState } from "react";
 import React from 'react';
-import { useState } from 'react'
+function Usestate() {
+  const [dark, setDark] = useState(false);
+  const [plus, setPlus] = useState(0);
 
-function usestate() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
 
-const [dark, setDark] = useState (false)
-    const [plus, setPlus] = useState (0)
+  // Add Todo
+  const addtodo = () => {
+    if (input.trim() === "") return;
+
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  // Delete Todo
+  const deletetodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
 
   return (
+    <div
+      style={{
+        backgroundColor: dark ? "black" : "darkgray",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        color: dark ? "white" : "black",
+        gap: "20px",
+      }}
+    >
+      {/* Counter */}
+      <h1>COUNTER</h1>
 
-    < div style= {{
-      backgroundColor: dark ? "black" : "darkgray",
-  height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      color: dark ? "darkgray" : "black",
-      
-    }}>
-        <h1>COUNTER</h1>       
-      <button onClick={() => setPlus(plus + 1)} > +</button>
-      <p>{plus}</p>
-            <button  onClick={() => setPlus(plus - 1)} > - </button>
+      <div>
+        <button onClick={() => setPlus(plus + 1)}>+</button>
 
-             < div className="" >
-                <button className='a'  onClick={() => setDark(!dark)}> Toggle </button>
+        <p>{plus}</p>
 
-             
+        <button onClick={() => setPlus(plus - 1)}>-</button>
+      </div>
+
+      {/* Dark Mode */}
+      <button onClick={() => setDark(!dark)}>
+        Toggle
+      </button>
+
+      {/* Todo List */}
+      <div>
+        <h1>TODO LIST</h1>
+
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter Todo"
+        />
+
+        <button onClick={addtodo}>ADD</button>
+
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              {todo}
+
+              <button onClick={() => deletetodo(index)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default usestate
+export default Usestate;
